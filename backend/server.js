e292914311c7ec49e571c7f79ec39eed74a33e60
KavyaReddy555 <kavya241996@gmail.com>
2023-05-18 08:57:55 +0530

    include socket in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const app = express();
-
+const cors = require("cors");
+const http = require("http");
 const dotenv = require("dotenv");
-
 const databaseConnect = require("./config/database");
 const authRouter = require("./routes/authRoute");
 const bodyParser = require("body-parser");
@@ -12,6 +11,9 @@ const messengerRoute = require("./routes/messengerRoute");
 dotenv.config({
   path: "backend/config/config.env",
 });
+
+const app = express();
+const server = http.createServer(app);
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -36,6 +38,114 @@ app.get("/", (req, res) => {
 });
 
 databaseConnect();
+/************************ */
+const io = require("socket.io")(server, {
+  cors: {
+    origins: "*:*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Origin, X-Requested-With, Content-Type, Accept"],
+    credentials: true,
+    handlePreFlightRequest: (req, res) => {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+        //      "Access-Control-Allow-Credentials": true,
+        "Referrer-Policy": "no-referrer",
+      });
+      res.end();
+    },
+  },
+});
+
+server.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
+
+let users = [];
+const addUser = (userId, socketId, userInfo) => {
+  const checkUser = users.some((u) => u.userId === userId);
+
+  if (!checkUser) {
+    users.push({ userId, socketId, userInfo });
+  }
+};
+const userRemove = (socketId) => {
+  users = users.filter((u) => u.socketId !== socketId);
+};
+
+const findFriend = (id) => {
+  return users.find((u) => u.userId === id);
+};
+
+const userLogout = (userId) => {
+  users = users.filter((u) => u.userId !== userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("Socket is connecting...");
+  socket.on("addUser", (userId, userInfo) => {
+    addUser(userId, socket.id, userInfo);
+    io.emit("getUser", users);
+
+    const us = users.filter((u) => u.userId !== userId);
+    const con = "new_user_add";
+    for (var i = 0; i < us.length; i++) {
+      socket.to(us[i].socketId).emit("new_user_add", con);
+    }
+  });
+  socket.on("sendMessage", (data) => {
+    console.log("send message socket", data);
+    const user = findFriend(data.reseverId);
+
+    if (user !== undefined) {
+      socket.to(user.socketId).emit("getMessage", data);
+    }
+  });
+
+  socket.on("messageSeen", (msg) => {
+    const user = findFriend(msg.senderId);
+    if (user !== undefined) {
+      socket.to(user.socketId).emit("msgSeenResponse", msg);
+    }
+  });
+
+  socket.on("delivaredMessage", (msg) => {
+    const user = findFriend(msg.senderId);
+    if (user !== undefined) {
+      socket.to(user.socketId).emit("msgDelivaredResponse", msg);
+    }
+  });
+  socket.on("seen", (data) => {
+    const user = findFriend(data.senderId);
+    if (user !== undefined) {
+      socket.to(user.socketId).emit("seenSuccess", data);
+    }
+  });
+
+  socket.on("typingMessage", (data) => {
+    const user = findFriend(data.reseverId);
+    if (user !== undefined) {
+      socket.to(user.socketId).emit("typingMessageGet", {
+        senderId: data.senderId,
+        reseverId: data.reseverId,
+        msg: data.msg,
+      });
+    }
+  });
+
+  socket.on("logout", (userId) => {
+    userLogout(userId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user is disconnect... ");
+    userRemove(socket.id);
+    io.emit("getUser", users);
+  });
+});
+/**************************/
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
