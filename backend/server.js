d0a069d3d4573551e89f4cb3ee1e368a41ec3492
KavyaReddy555 <kavya241996@gmail.com>
2023-05-18 21:10:10 +0530

    change port

--- backend/server.js
+++ backend/server.js
@@ -145,10 +145,10 @@ io.on("connection", (socket) => {
 });
 /**************************/
 
-server.listen(8000, () => {
+server.listen(5000, () => {
   console.log("Server listening on port 8000");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
